0b893d5560a6dc733791947cb6778f93453c7ebe
raccoon91 <qsx314@gmail.com>
2023-10-21 16:58:39 +0900

    feat: setup eslint, prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "off",
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 120,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+  endOfLine: "auto",
+};
