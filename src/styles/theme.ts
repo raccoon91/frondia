5d9c5c3db0054e7cf5051230403dc9342075329d
raccoon91 <qsx314@gmail.com>
2023-10-21 17:12:56 +0900

    feat: setup chakra ui & add font

---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  fonts: {
+    heading: `'Noto Sans KR Variable', sans-serif`,
+    body: `'Noto Sans KR Variable', sans-serif`,
+  },
+});
