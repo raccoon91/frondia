771687dafdc108cc45ff57223554bf9fba425d35
raccoon91 <qsx314@gmail.com>
2023-10-22 19:32:42 +0900

    style: add scrollbar style

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,8 +3,8 @@ import { extendTheme, withDefaultColorScheme, theme as baseTheme } from "@chakra
 export const theme = extendTheme(
   {
     fonts: {
-      heading: `'Noto Sans KR Variable', sans-serif`,
-      body: `'Noto Sans KR Variable', sans-serif`,
+      heading: "'Noto Sans KR Variable', sans-serif",
+      body: "'Noto Sans KR Variable', sans-serif",
     },
     colors: {
       brand: baseTheme.colors.yellow,
@@ -15,6 +15,24 @@ export const theme = extendTheme(
         background: baseTheme.colors.yellow[50],
         surface: "#fcfcf5",
         border: baseTheme.colors.gray[200],
+      },
+    },
+    styles: {
+      global: {
+        "*": {
+          "&::-webkit-scrollbar": {
+            width: "4px",
+            height: "4px",
+          },
+          "&::-webkit-scrollbar-track": {
+            width: "4px",
+            height: "4px",
+          },
+          "&::-webkit-scrollbar-thumb": {
+            background: baseTheme.colors.yellow[500],
+            borderRadius: "3px",
+          },
+        },
       },
     },
   },
