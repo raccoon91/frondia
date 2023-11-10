5414ec6f3121bcd9c4e86b80b6c8dff3b05a597b
raccoon91 <qsx314@gmail.com>
2023-11-10 22:51:31 +0900

    feat: add card component right action

--- src/components/base/Card.tsx
+++ src/components/base/Card.tsx
@@ -1,16 +1,19 @@
+import { FC, ReactNode } from "react";
 import { BoxProps, chakra } from "@chakra-ui/react";
-import { FC } from "react";
 
 interface ICardProps extends BoxProps {
   title?: string;
+  rightAction?: ReactNode;
 }
 
-export const Card: FC<ICardProps> = ({ title, children, ...props }) => {
+export const Card: FC<ICardProps> = ({ title, children, rightAction, ...props }) => {
   return (
     <chakra.div border="1px solid" borderColor="border" rounded="md" shadow="md" bg="surface" {...props}>
-      {title ? (
-        <chakra.div p="12px 24px" borderBottom="1px solid" borderColor="border" fontWeight="600">
-          {title}
+      {title || rightAction ? (
+        <chakra.div display="flex" alignItems="center" p="12px 16px" borderBottom="1px solid" borderColor="border">
+          {title && <chakra.p fontWeight="600">{title}</chakra.p>}
+
+          {rightAction && <chakra.div ml="auto">{rightAction}</chakra.div>}
         </chakra.div>
       ) : null}
 
