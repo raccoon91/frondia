e4cd6d8d0bca9eab1f8a74a8b12d22347c74b204
raccoon91 <dev.beomseok@gmail.com>
2025-03-18 21:00:50 +0900

    feat: integrate persistence into transaction store using Zustand middleware for improved state management

--- src/stores/transaction.store.ts
+++ src/stores/transaction.store.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { devtools } from "zustand/middleware";
+import { createJSONStorage, devtools, persist } from "zustand/middleware";
 
 import { supabase } from "@/lib/supabase";
 import dayjs from "dayjs";
@@ -27,263 +27,277 @@ interface TransactionState {
 }
 
 export const useTransactionStore = create<TransactionState>()(
-  devtools((set, get) => ({
-    currencies: [],
-    transactionTypes: [],
-    categories: [],
+  devtools(
+    persist(
+      (set, get) => ({
+        currencies: [],
+        transactionTypes: [],
+        categories: [],
 
-    transactionDatasets: [],
+        transactionDatasets: [],
 
-    getCurrencies: async () => {
-      try {
-        const { data } = await supabase.from("currencies").select("*");
+        getCurrencies: async () => {
+          try {
+            const { data } = await supabase.from("currencies").select("*");
 
-        set({ currencies: data ?? [] }, false, "getCurrencies");
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    getTransactionTypes: async () => {
-      try {
-        const { data } = await supabase.from("transaction_types").select("*");
+            set({ currencies: data ?? [] }, false, "getCurrencies");
+          } catch (error) {
+            console.error(error);
+          }
+        },
+        getTransactionTypes: async () => {
+          try {
+            const { data } = await supabase.from("transaction_types").select("*");
 
-        set({ transactionTypes: data ?? [] }, false, "getTransactionTypes");
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    getCategories: async () => {
-      try {
-        const { data } = await supabase.from("categories").select("*");
+            set({ transactionTypes: data ?? [] }, false, "getTransactionTypes");
+          } catch (error) {
+            console.error(error);
+          }
+        },
+        getCategories: async () => {
+          try {
+            const { data } = await supabase.from("categories").select("*");
 
-        set({ categories: data ?? [] }, false, "getCategories");
-      } catch (error) {
-        console.error(error);
-      }
-    },
+            set({ categories: data ?? [] }, false, "getCategories");
+          } catch (error) {
+            console.error(error);
+          }
+        },
 
-    getTransactions: async () => {
-      try {
-        const { data } = await supabase
-          .from("transactions")
-          .select("*, currency: currency_id (*), transactionType: type_id (*), category: category_id (*)")
-          .order("date", { ascending: false });
+        getTransactions: async () => {
+          try {
+            const { data } = await supabase
+              .from("transactions")
+              .select("*, currency: currency_id (*), transactionType: type_id (*), category: category_id (*)")
+              .order("date", { ascending: false });
 
-        const transactionDatasets =
-          data?.map((transaction) => ({
-            id: transaction.id,
-            status: "done",
-            checked: false,
-            date: transaction.date,
-            transactionType: transaction.transactionType,
-            category: transaction.category,
-            currency: transaction.currency,
-            memo: transaction.memo,
-            amount: transaction.amount,
-          })) ?? [];
+            const transactionDatasets =
+              data?.map((transaction) => ({
+                id: transaction.id,
+                status: "done",
+                checked: false,
+                date: transaction.date,
+                transactionType: transaction.transactionType,
+                category: transaction.category,
+                currency: transaction.currency,
+                memo: transaction.memo,
+                amount: transaction.amount,
+              })) ?? [];
 
-        set({ transactionDatasets }, false, "getTransactions");
-      } catch (error) {
-        console.error(error);
-      }
-    },
+            set({ transactionDatasets }, false, "getTransactions");
+          } catch (error) {
+            console.error(error);
+          }
+        },
 
-    addTransaction: () => {
-      const currencies = get().currencies;
-      const transactionTypes = get().transactionTypes;
-      const categories = get().categories;
+        addTransaction: () => {
+          const currencies = get().currencies;
+          const transactionTypes = get().transactionTypes;
+          const categories = get().categories;
 
-      set(
-        (prev) => ({
-          transactionDatasets: [
-            {
-              id: dayjs().unix(),
-              status: "new",
-              checked: false,
-              date: dayjs().format("YYYY-MM-DD HH:mm"),
-              amount: 0,
-              memo: null,
-
-              currencies,
-              transactionTypes,
-              categories,
-            },
-            ...prev.transactionDatasets,
-          ],
-        }),
-        false,
-        "addTransaction",
-      );
-    },
-    deleteTransaction: async () => {
-      try {
-        const transactionDatasets = get().transactionDatasets;
-
-        const { deleted, filtered } = transactionDatasets.reduce<{
-          deleted: TransactionData[];
-          filtered: TransactionData[];
-        }>(
-          (acc, cur) => {
-            if (cur.status === "new" && cur.checked) return acc;
-
-            if (cur.checked) {
-              acc.deleted.push(cur);
-            } else {
-              acc.filtered.push(cur);
-            }
-
-            return acc;
-          },
-          { deleted: [], filtered: [] },
-        );
-
-        if (deleted.length) {
-          await supabase
-            .from("transactions")
-            .delete()
-            .in(
-              "id",
-              deleted.map((datasets) => datasets.id),
-            );
-        }
-
-        set({ transactionDatasets: filtered }, false, "deleteTransaction");
-      } catch (error) {
-        console.error(error);
-      }
-    },
-
-    editTransaction: (rowIndex: number) => {
-      const currencies = get().currencies;
-      const transactionTypes = get().transactionTypes;
-      const categories = get().categories;
-
-      set(
-        (prev) => ({
-          transactionDatasets: prev.transactionDatasets.map((transactionDataset, index) => {
-            if (index === rowIndex) {
-              return {
-                id: transactionDataset.id,
-                status: "edit",
-                checked: transactionDataset.checked,
-                date: transactionDataset.date,
-                currency: transactionDataset.currency,
-                transactionType: transactionDataset.transactionType,
-                category: transactionDataset.category,
-                amount: transactionDataset.amount,
-                memo: transactionDataset.memo,
-
-                currencies,
-                transactionTypes,
-                categories,
-              };
-            }
-
-            return transactionDataset;
-          }),
-        }),
-        false,
-        "editTransaction",
-      );
-    },
-    checkTransaction: (rowIndex: number, value: boolean) => {
-      set(
-        (prev) => ({
-          transactionDatasets: prev.transactionDatasets.map((data, dataIndex) => {
-            if (dataIndex !== rowIndex) return data;
-
-            return { ...data, checked: value };
-          }),
-        }),
-        false,
-        "checkTransaction",
-      );
-    },
-    changeTransaction: (rowIndex: number, columnName: string, value: number | string) => {
-      set(
-        (prev) => ({
-          transactionDatasets: prev.transactionDatasets.map((data, dataIndex) => {
-            if (dataIndex !== rowIndex) return data;
-
-            if (columnName === "transactionType") {
-              const transactionType = get().transactionTypes?.find(
-                (transactionType) => transactionType.id.toString() === value.toString(),
-              );
-              const categories = get().categories.filter((category) => category.type_id === transactionType?.id);
-
-              return { ...data, transactionType, category: undefined, categories };
-            } else if (columnName === "category") {
-              const category = get().categories.find((category) => category.id.toString() === value.toString());
-
-              return { ...data, category };
-            } else if (columnName === "currency") {
-              const currency = get().currencies.find((currency) => currency.id.toString() === value.toString());
-
-              return { ...data, currency };
-            }
-
-            return { ...data, [columnName]: value };
-          }),
-        }),
-        false,
-        "changeTransaction",
-      );
-    },
-    upsertTransaction: async (rowIndex: number) => {
-      try {
-        const transaction = get().transactionDatasets?.[rowIndex];
-
-        if (
-          !transaction ||
-          !transaction.date ||
-          !transaction.transactionType ||
-          !transaction.category ||
-          !transaction.currency
-        )
-          return;
-
-        const { data: newTransaction } = await supabase
-          .from("transactions")
-          .upsert({
-            id: transaction.status === "new" ? undefined : transaction.id,
-            date: transaction.date,
-            type_id: transaction.transactionType.id,
-            category_id: transaction.category.id,
-            currency_id: transaction.currency.id,
-            memo: transaction.memo,
-            amount: transaction.amount,
-          })
-          .select("*, currency: currency_id (*), transactionType: type_id (*), category: category_id (*)")
-          .maybeSingle();
-
-        if (!newTransaction) return;
-
-        set(
-          (prev) => ({
-            transactionDatasets: prev.transactionDatasets.map((data, index) => {
-              if (rowIndex === index) {
-                return {
-                  id: newTransaction.id,
-                  status: "done",
+          set(
+            (prev) => ({
+              transactionDatasets: [
+                {
+                  id: dayjs().unix(),
+                  status: "new",
                   checked: false,
-                  date: newTransaction.date,
-                  transactionType: newTransaction.transactionType,
-                  category: newTransaction.category,
-                  currency: newTransaction.currency,
-                  memo: newTransaction.memo,
-                  amount: newTransaction.amount,
-                };
-              }
+                  date: dayjs().format("YYYY-MM-DD HH:mm"),
+                  amount: 0,
+                  memo: null,
 
-              return data;
+                  currencies,
+                  transactionTypes,
+                  categories,
+                },
+                ...prev.transactionDatasets,
+              ],
             }),
-          }),
-          false,
-          "upsertTransaction",
-        );
-      } catch (error) {
-        console.error(error);
-      }
-    },
-  })),
+            false,
+            "addTransaction",
+          );
+        },
+        deleteTransaction: async () => {
+          try {
+            const transactionDatasets = get().transactionDatasets;
+
+            const { deleted, filtered } = transactionDatasets.reduce<{
+              deleted: TransactionData[];
+              filtered: TransactionData[];
+            }>(
+              (acc, cur) => {
+                if (cur.status === "new" && cur.checked) return acc;
+
+                if (cur.checked) {
+                  acc.deleted.push(cur);
+                } else {
+                  acc.filtered.push(cur);
+                }
+
+                return acc;
+              },
+              { deleted: [], filtered: [] },
+            );
+
+            if (deleted.length) {
+              await supabase
+                .from("transactions")
+                .delete()
+                .in(
+                  "id",
+                  deleted.map((datasets) => datasets.id),
+                );
+            }
+
+            set({ transactionDatasets: filtered }, false, "deleteTransaction");
+          } catch (error) {
+            console.error(error);
+          }
+        },
+
+        editTransaction: (rowIndex: number) => {
+          const currencies = get().currencies;
+          const transactionTypes = get().transactionTypes;
+          const categories = get().categories;
+
+          set(
+            (prev) => ({
+              transactionDatasets: prev.transactionDatasets.map((transactionDataset, index) => {
+                if (index === rowIndex) {
+                  return {
+                    id: transactionDataset.id,
+                    status: "edit",
+                    checked: transactionDataset.checked,
+                    date: transactionDataset.date,
+                    currency: transactionDataset.currency,
+                    transactionType: transactionDataset.transactionType,
+                    category: transactionDataset.category,
+                    amount: transactionDataset.amount,
+                    memo: transactionDataset.memo,
+
+                    currencies,
+                    transactionTypes,
+                    categories,
+                  };
+                }
+
+                return transactionDataset;
+              }),
+            }),
+            false,
+            "editTransaction",
+          );
+        },
+        checkTransaction: (rowIndex: number, value: boolean) => {
+          set(
+            (prev) => ({
+              transactionDatasets: prev.transactionDatasets.map((data, dataIndex) => {
+                if (dataIndex !== rowIndex) return data;
+
+                return { ...data, checked: value };
+              }),
+            }),
+            false,
+            "checkTransaction",
+          );
+        },
+        changeTransaction: (rowIndex: number, columnName: string, value: number | string) => {
+          set(
+            (prev) => ({
+              transactionDatasets: prev.transactionDatasets.map((data, dataIndex) => {
+                if (dataIndex !== rowIndex) return data;
+
+                if (columnName === "transactionType") {
+                  const transactionType = get().transactionTypes?.find(
+                    (transactionType) => transactionType.id.toString() === value.toString(),
+                  );
+                  const categories = get().categories.filter((category) => category.type_id === transactionType?.id);
+
+                  return { ...data, transactionType, category: undefined, categories };
+                } else if (columnName === "category") {
+                  const category = get().categories.find((category) => category.id.toString() === value.toString());
+
+                  return { ...data, category };
+                } else if (columnName === "currency") {
+                  const currency = get().currencies.find((currency) => currency.id.toString() === value.toString());
+
+                  return { ...data, currency };
+                }
+
+                return { ...data, [columnName]: value };
+              }),
+            }),
+            false,
+            "changeTransaction",
+          );
+        },
+        upsertTransaction: async (rowIndex: number) => {
+          try {
+            const transaction = get().transactionDatasets?.[rowIndex];
+
+            if (
+              !transaction ||
+              !transaction.date ||
+              !transaction.transactionType ||
+              !transaction.category ||
+              !transaction.currency
+            )
+              return;
+
+            const { data: newTransaction } = await supabase
+              .from("transactions")
+              .upsert({
+                id: transaction.status === "new" ? undefined : transaction.id,
+                date: transaction.date,
+                type_id: transaction.transactionType.id,
+                category_id: transaction.category.id,
+                currency_id: transaction.currency.id,
+                memo: transaction.memo,
+                amount: transaction.amount,
+              })
+              .select("*, currency: currency_id (*), transactionType: type_id (*), category: category_id (*)")
+              .maybeSingle();
+
+            if (!newTransaction) return;
+
+            set(
+              (prev) => ({
+                transactionDatasets: prev.transactionDatasets.map((data, index) => {
+                  if (rowIndex === index) {
+                    return {
+                      id: newTransaction.id,
+                      status: "done",
+                      checked: false,
+                      date: newTransaction.date,
+                      transactionType: newTransaction.transactionType,
+                      category: newTransaction.category,
+                      currency: newTransaction.currency,
+                      memo: newTransaction.memo,
+                      amount: newTransaction.amount,
+                    };
+                  }
+
+                  return data;
+                }),
+              }),
+              false,
+              "upsertTransaction",
+            );
+          } catch (error) {
+            console.error(error);
+          }
+        },
+      }),
+      {
+        name: "transaction-store",
+        storage: createJSONStorage(() => sessionStorage),
+        partialize: (state) => ({
+          currencies: state.currencies,
+          transactionTypes: state.transactionTypes,
+          categories: state.categories,
+          transactionDatasets: state.transactionDatasets,
+        }),
+      },
+    ),
+  ),
 );
