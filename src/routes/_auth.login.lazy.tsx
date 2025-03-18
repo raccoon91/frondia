64d2c850597e9c72f3b428daa28283600dc8c1df
raccoon91 <dev.beomseok@gmail.com>
2025-03-18 13:02:12 +0900

    feat: add UI components for form handling, including Button, Input, and Form elements, and implement login functionality with validation

--- src/routes/_auth.login.lazy.tsx
+++ src/routes/_auth.login.lazy.tsx
@@ -1,9 +1,92 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
+import { createLazyFileRoute, useNavigate } from "@tanstack/react-router";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { loginFormSchema } from "@/schema/auth.schema";
+import { cn } from "@/lib/utils";
+import { supabase } from "@/lib/supabase";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Form, FormMessage, FormControl, FormLabel, FormItem, FormField } from "@/components/ui/form";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleSubmitLogin = async (formdata: z.infer<typeof loginFormSchema>) => {
+    const { data } = await supabase.auth.signInWithPassword({ email: formdata.email, password: formdata.password });
+
+    if (data.user) navigate({ to: "/" });
+  };
+
   return (
-    <div>
-      <p>Login Page</p>
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <Form {...form}>
+              <form className={cn("flex flex-col gap-6")} onSubmit={form.handleSubmit(handleSubmitLogin)}>
+                <div className="flex flex-col items-center gap-2 text-center">
+                  <h1 className="text-2xl font-bold">Login to your account</h1>
+                  <p className="text-balance text-sm text-muted-foreground">
+                    Enter your email below to login to your account
+                  </p>
+                </div>
+
+                <div className="grid gap-6">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl>
+                          <Input type="email" autoFocus {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl>
+                          <Input type="password" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <Button type="submit" className="w-full">
+                    Login
+                  </Button>
+                </div>
+
+                <div className="text-center text-sm">
+                  Don&apos;t have an account?{" "}
+                  <a href="#" className="underline underline-offset-4">
+                    Sign up
+                  </a>
+                </div>
+              </form>
+            </Form>
+          </div>
+        </div>
+      </div>
+
+      <div className="relative hidden bg-muted lg:block"></div>
     </div>
   );
 };
