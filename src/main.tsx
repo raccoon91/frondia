362942fb5d2ea181f223a4c9ee61de9c4071c0d0
raccoon91 <dev.beomseok@gmail.com>
2025-03-20 23:07:06 +0900

    refactor: move router type declaration to a separate type definition file for better organization and maintainability

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,6 @@ import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({ routeTree });
 
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
-}
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
