b8799835147530848876a47fac11cdaab6306889
raccoon91 <dev.beomseok@gmail.com>
2025-03-21 11:51:50 +0900

    style: update Select and Table components with new hover and background styles for improved user interaction and visual consistency

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -22,7 +22,7 @@ function TableFooter({ className, ...props }: React.ComponentProps<"tfoot">) {
   return (
     <tfoot
       data-slot="table-footer"
-      className={cn("bg-muted/50 border-t font-medium [&>tr]:last:border-b-0", className)}
+      className={cn("bg-card border-t font-medium [&>tr]:last:border-b-0", className)}
       {...props}
     />
   );
@@ -32,7 +32,7 @@ function TableRow({ className, ...props }: React.ComponentProps<"tr">) {
   return (
     <tr
       data-slot="table-row"
-      className={cn("hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors", className)}
+      className={cn("hover:bg-card data-[state=selected]:bg-card border-b transition-colors", className)}
       {...props}
     />
   );
@@ -43,7 +43,7 @@ function TableHead({ className, ...props }: React.ComponentProps<"th">) {
     <th
       data-slot="table-head"
       className={cn(
-        "text-muted-foreground h-10 px-2 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+        "text-card-foreground h-10 px-2 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
         className,
       )}
       {...props}
@@ -66,7 +66,7 @@ function TableCell({ className, ...props }: React.ComponentProps<"td">) {
 
 function TableCaption({ className, ...props }: React.ComponentProps<"caption">) {
   return (
-    <caption data-slot="table-caption" className={cn("text-muted-foreground mt-4 text-sm", className)} {...props} />
+    <caption data-slot="table-caption" className={cn("text-card-foreground mt-4 text-sm", className)} {...props} />
   );
 }
 
